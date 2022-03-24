61591d5495ef72fd48924d1ea6b6b2220c1a8da2
fahad-shawal <fahadshawal@gmail.com>
2022-03-24 23:01:04 +0500

    add more randomness in token generation

--- Rishta_App/backend/users/tokens.py
+++ Rishta_App/backend/users/tokens.py
@@ -4,7 +4,13 @@ import random
 
 class TokenGenerator():
     @staticmethod
-    def generate_token(size=4, chars=string.ascii_uppercase + string.digits):
-        return ''.join(random.choice(chars) for _ in range(size))
+    def generate_token(size=4):
+        chars = string.ascii_uppercase
+        numbers = string.digits
+        total_digits = random.choice([num for num in range(size-1)])
+        chars_collection = [random.choice(chars) for _ in range(size-total_digits)]
+        digits_collection = [random.choice(numbers) for _ in range(total_digits)]
+
+        return ''.join(chars_collection + digits_collection)
 
 
