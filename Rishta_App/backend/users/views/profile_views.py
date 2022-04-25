f516db7e108f91c3a4a9a76c0b03a84b9a788aff
Luqman-Ud-Din <luqmanuddinm@gmail.com>
2022-04-25 11:29:28 +0500

    chore: created DateService and profile views updated query parameters

--- Rishta_App/backend/users/views/profile_views.py
+++ Rishta_App/backend/users/views/profile_views.py
@@ -1,13 +1,11 @@
-from datetime import datetime
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from backend.users.models import ProfileView
 from backend.users.serializers import ProfileViewSerializer
+from services.date_service import DateService
 
-
-DATETIME_FORMATE = '%Y-%m-%d'
 
 class ProfileViewAPIViewSet(ModelViewSet):
     permission_classes = (IsAuthenticated,)
@@ -15,32 +13,32 @@ class ProfileViewAPIViewSet(ModelViewSet):
     serializer_class = ProfileViewSerializer
 
     def get_queryset(self):
+        queryset = self.queryset
         start_date = self.request.query_params.get('start')
         end_date = self.request.query_params.get('end')
-        
-        if not (start_date or end_date):
-            return self.queryset.all()
 
-        queryset = self.queryset
+        if not (start_date or end_date or self.action != 'list'):
+            return queryset.all()
+
         if start_date:
-            start_date = datetime.strptime(start_date, DATETIME_FORMATE)
+            start_date = DateService.from_timestamp(start_date)
             queryset = queryset.filter(created_at__gte=start_date)
-       
+
         if end_date:
-            end_date = datetime.strptime(end_date, DATETIME_FORMATE)
+            end_date = DateService.from_timestamp(end_date)
             queryset = queryset.filter(created_at__lte=end_date)
-        
+
         return queryset
 
     @extend_schema(
         parameters=[
             OpenApiParameter(
-                name='start datge', location=OpenApiParameter.QUERY,
-                description='Starting date (YYYY-MM-DD)', required=False, type=str
+                name='start_date', location=OpenApiParameter.QUERY,
+                description='start date UTC unix timestamp', required=False, type=str
             ),
             OpenApiParameter(
-                name='end date', location=OpenApiParameter.QUERY,
-                description='Ending date (YYYY-MM-DD)', required=False, type=str
+                name='end_date', location=OpenApiParameter.QUERY,
+                description='end date UTC unix timestamp', required=False, type=str
             )
         ],
     )
